f0c569926f74d2f2c8c5799083fb62f3a003bc73
jackychenb <jackychenb@jackychenbtekiMacBook-Pro.local>
2017-01-17 16:08:56 +0800

    Adjust scripts

--- etc/install2localrepository.bat
+++ etc/install2localrepository.bat
@@ -4,6 +4,6 @@ rmdir /S /Q "%USERPROFILE%\.m2\repository\com\omega\demo\omega-demo-api"
 
 
 
-mvn install:install-file -DgroupId=com.omega.demo -DartifactId=omega-demo-api -Dversion=0.1 \
+call mvn install:install-file -DgroupId=com.omega.demo -DartifactId=omega-demo-api -Dversion=0.1 \
   -Dfile=lib/omega-demo-api-0.1.jar  -DgeneratePom=true -Dpackaging=jar
 
